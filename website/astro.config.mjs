1a12eae305293d85b9383d6bf9690ffa1887a135
Adithya Krishna <aadithya794@gmail.com>
2026-08-07 16:23:16 +0530

    chore: updated sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://zonely.adikris.in",
   outDir: "./site-dist",
+  integrations: [sitemap()],
 });
